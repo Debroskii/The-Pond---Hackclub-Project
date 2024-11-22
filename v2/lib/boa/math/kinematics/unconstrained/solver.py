929a562087636dd1a20dc6abbca17c26207f37ac
E-DeBusk <debusk.elijah@gmail.com>
2024-11-21 22:46:02 -0800

    Miscelaneous changes to the IK solver to reduce the tearing

--- v2/lib/boa/math/kinematics/unconstrained/solver.py
+++ v2/lib/boa/math/kinematics/unconstrained/solver.py
@@ -9,14 +9,14 @@ class UIKinematicsSolver:
         transformation = follower.trailing - leader.trailing
         follower.angle = math.atan2(transformation.y, transformation.x)
         
-        follower.trailing = leader.trailing + transformation
         follower.leading = leader.trailing
+        follower.trailing = leader.trailing + transformation
         follower.solveTrailing()
         
     def solveToVector(follower: Link, leader: pygame.Vector2):
         transformation = follower.trailing - leader
         follower.angle = math.atan2(transformation.y, transformation.x)
         
-        follower.trailing = leader + transformation
         follower.leading = leader
+        follower.trailing = leader + transformation
         follower.solveTrailing()
