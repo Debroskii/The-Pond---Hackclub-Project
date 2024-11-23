73701d6b0da121b637a197d220af8eab93bcf3e1
E-DeBusk <debusk.elijah@gmail.com>
2024-11-22 19:14:13 -0800

    Finished the basic PathEntity logic for generation and motion

--- v2/game/entity/fish.py
+++ v2/game/entity/fish.py
@@ -1,7 +1,15 @@
+from entity.path_entity import PathEntity
 from logic.trait_collection import TraitCollection
 
 class Fish:
     def __init__(self, color_config, scale: float, traits: TraitCollection):
+        self.path_entity = PathEntity()
         self.color_config = color_config
         self.scale = scale
         self.traits = traits
+        
+    def update(self):
+        self.path_entity.loop()
+        
+    def draw(self):
+        pass
--- v2/game/entity/path_entity.py
+++ v2/game/entity/path_entity.py
@@ -3,28 +3,28 @@ import random
 import pygame
 
 from config.global_config import GLOBALCONFIG
-from game import ThePond
+import game_manager
 
-
-class PathGenerator:
+class PathEntity:
     def __init__(self):
         self.pos = pygame.Vector2(random.randint(0, GLOBALCONFIG.window_width), random.randint(0, GLOBALCONFIG.window_height))
         self.target = pygame.Vector2((GLOBALCONFIG.window_width / 2) + random.randint(-100, 100), (GLOBALCONFIG.window_height / 2) + random.randint(-100, 100))
         self.heading = 0
-        self.speed = 1
+        self.speed = 2
         
     def loop(self):
+        print(self.heading)
         self.heading = math.atan2((self.target - self.pos).y, (self.target - self.pos).x)
-        self.pos += pygame.Vector2(math.cos(self.heading * math.pi / 180) * self.speed, math.sin(self.heading * math.pi / 180) * self.speed)
+        self.pos += pygame.Vector2(math.cos(self.heading) * self.speed, math.sin(self.heading) * self.speed)
         
         if (self.target - self.pos).magnitude() <= 2:
-            self.heading += random.randint(-random.randint(30, 55), random.randint(30, 55))
+            self.heading += random.uniform(-random.uniform(math.pi / 6, math.pi / 4), random.uniform(math.pi / 6, math.pi / 4))
             self.target.update(
                 self.pos.x + math.cos(self.heading) * 50, 
                 self.pos.y + math.sin(self.heading) * 50
             )
             
-            if ThePond.out_of_logic_bounds(self.pos):
+            if game_manager.ThePond.out_of_logic_bounds(self.pos):
                 self.target.update((GLOBALCONFIG.window_width / 2) + random.randint(-100, 100), (GLOBALCONFIG.window_height / 2) + random.randint(-100, 100))
                 
     def debug_draw(self, surface):
