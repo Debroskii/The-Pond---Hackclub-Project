674370a225de45babb41a34e10716d465fba534f
E-DeBusk <debusk.elijah@gmail.com>
2024-11-26 21:13:24 -0800

    Removed the motive type; it's useless

--- v2/game/logic/motive/Motive.py
+++ v2/game/logic/motive/Motive.py
@@ -1,14 +1,8 @@
-from enum import Enum
-
 import pygame
 
-class MotiveType(Enum):
-    FOOD:0 
-
 class Motive:
-    def __init__(self, type: MotiveType, location: pygame.Vector2, radius: float):
+    def __init__(self, location: pygame.Vector2, radius: float):
         self.lifetime = 0
-        self.type = type
         self.location = location
         self.radius = radius
     
