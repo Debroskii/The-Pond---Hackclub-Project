42f3b394629b20d5c5171dff81373237ae3262a2
E-DeBusk <debusk.elijah@gmail.com>
2024-11-26 21:13:59 -0800

    added a debug draw function and edited the random motive to fix its functions

--- v2/game/logic/motive/PondMotives.py
+++ v2/game/logic/motive/PondMotives.py
@@ -2,21 +2,29 @@ import math
 import random
 
 import pygame
-from logic.motive.Motive import Motive
+from game.logic.motive.Motive import Motive
 
 
 class PondMotives:
-    motives = []
+    motives = [Motive(pygame.Vector2(0, 0), 10)]
     
     def add(motive: Motive):
         PondMotives.motives.append(motive)
         
     def generateTarget():
-        motive = PondMotives.motives[len(PondMotives.motives) - 1]
+        if len(PondMotives.motives) == 0:
+            return
+        motive = random.choice(list(PondMotives.motives))
         angle = (random.randint(0, 200) / 100) * math.pi
-        radius = motive.radius() * (random.randint(0, 100) / 100)
+        radius = motive.radius * (random.randint(0, 100) / 100)
         target_delta = pygame.Vector2(
             math.cos(angle) * radius,
             math.sin(angle) * radius
         )
         return motive.location + target_delta
+    
+    def debug_draw(surface):
+        for motive in PondMotives.motives:
+            pygame.draw.circle(surface, (0, 255, 0), motive.location, 5)
+            pygame.draw.circle(surface, (255, 255, 255), motive.location, motive.radius, 1)
+            
