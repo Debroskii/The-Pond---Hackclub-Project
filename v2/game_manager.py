94007d01b2799a87b6069b7f8a7323aa90a6ddbd
E-DeBusk <debusk.elijah@gmail.com>
2024-11-27 18:05:43 -0800

    Added debug code to store multiple path entities

--- v2/game_manager.py
+++ v2/game_manager.py
@@ -2,18 +2,24 @@ from game.entity.fish import Fish
 from lib.boa.art.draw import draw
 from lib.boa.math.kinematics.unconstrained.chain import UIKinematicsChain
 from config.global_config import GLOBALCONFIG
-from game.entity.path_entity import PathEntity
+from game.entity.path_entity import EntityType, PathEntity
 from game.entity.group_path_entity import GroupPathEntity
+from game.entity.food_objective_entity import FoodObjectiveEntity
 
 class GameManager:
-    debug_path_entity = GroupPathEntity()
+    debug_path_entities = []
+    
+    for i in range(1):
+      debug_path_entities.append(PathEntity(EntityType.INDIVIDUAL))
     
     def update(timestamp):
-        GameManager.debug_path_entity.loop(timestamp)
-        # GameManager.debug_path_entity.console_debugging()
+      for entity in GameManager.debug_path_entities:
+        entity.loop(timestamp)
+        # entity.console_debugging()
     
     def draw(surface):
-        GameManager.debug_path_entity.draw(surface)
+      for entity in GameManager.debug_path_entities:
+        entity.debug_draw(surface)
         
     def out_of_logic_bounds(object):
         if object.x > GLOBALCONFIG.window_width + 200 or object.y > GLOBALCONFIG.window_height + 200:
