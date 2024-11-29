ff23b64e75cdd91fef7c3a9795f250ca58715ffd
E-DeBusk <debusk.elijah@gmail.com>
2024-11-28 21:23:57 -0800

    Changed how the pattern is stored to only generate once instead of per-frame

--- v2/lib/boa/art/pattern.py
+++ v2/lib/boa/art/pattern.py
@@ -5,17 +5,16 @@ import pygame
 
 
 class Pattern:
-    def __init__(self, resolution):
+    def __init__(self, resolution, base_color, pattern_color):
         self.res = resolution
         self.seed = random.randint(0, 1000000)
         self.points = []
         
-        for i in range(15):
+        for i in range(150):
             self.points.append(pygame.Vector2(random.randint(0, pygame.display.get_window_size()[0]), random.randint(0, pygame.display.get_window_size()[1])))
-        
-    def draw(self, surface, base_color, pattern_color):
-        pattern_surface = pygame.Surface(pygame.display.get_window_size())
-        pattern_surface.fill(base_color)
+            
+        self.pattern_surface = pygame.Surface(pygame.display.get_window_size())
+        self.pattern_surface.fill(base_color)
         
         for row in range(self.res):
             for col in range(self.res):
@@ -27,9 +26,11 @@ class Pattern:
                     if (point - position).magnitude() < closest_point_magnitude:
                         closest_point_magnitude = (point - position).magnitude()
                 
-                scale = 1 - max(0, min(closest_point_magnitude * 0.025 * abs(row / (col + 1)), 1))
+                scale = 1 - max(0, min(closest_point_magnitude * 0.1 * abs(math.sin(row * col)), 1))
                 
-                pygame.draw.circle(pattern_surface, pattern_color, position - pygame.Vector2(unit_width / 2, unit_height / 2), unit_width * 2 * scale)
+                pygame.draw.circle(self.pattern_surface, pattern_color, position - pygame.Vector2(unit_width / 2, unit_height / 2), unit_width * 2 * scale)
                 
-        # surface.blit(pattern_surface, (0, 0))
-        return pattern_surface
+    def getPattern(self, offset):
+        return_surface = pygame.Surface((900, 900))
+        return_surface.blit(self.pattern_surface, offset)
+        return return_surface
