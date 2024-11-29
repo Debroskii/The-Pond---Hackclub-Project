fa685ca325e7f783b54c45566313d5235f3bdee5
E-DeBusk <debusk.elijah@gmail.com>
2024-11-28 21:22:29 -0800

    Removed the unused .init implementation

--- v2/app.py
+++ v2/app.py
@@ -1,8 +1,10 @@
+import math
 import random
 import pygame
 
 from config.global_config import GLOBALCONFIG
 from game_manager import GameManager
+from lib.boa.art.draw import draw
 
 pygame.init()
 pygame.display.set_caption("The Pond - HackClub Project")
@@ -10,17 +12,15 @@ pygame.display.set_caption("The Pond - HackClub Project")
 main_surface = pygame.display.set_mode((GLOBALCONFIG.window_width, GLOBALCONFIG.window_height))
 running: bool = True
 
-GameManager.init()
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    main_surface.fill(0)
-    
-    GameManager.update(pygame.time.get_ticks())
-    GameManager.draw(main_surface)
+    main_surface.fill(GLOBALCONFIG.window_color)
+        
+    # GameManager.update(pygame.time.get_ticks())
+    # GameManager.draw(main_surface)
         
     pygame.display.update()
     pygame.time.Clock().tick(120)
