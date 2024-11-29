03d2ab320064cb1ce4f164f4af5eec99ae889e99
E-DeBusk <debusk.elijah@gmail.com>
2024-11-28 21:25:13 -0800

    Re-added the rotated ellipse and rectangle functions from the original version

--- v2/lib/boa/art/draw.py
+++ v2/lib/boa/art/draw.py
@@ -146,3 +146,25 @@ class draw:
             points.append(pygame.Vector2(position.x + math.cos(angle + span_amount) * radius, position.y + math.sin(angle + span_amount) * radius))
             
         pygame.draw.polygon(surface, color, points)
+        
+    def rotated_rect(surface, color: tuple, rect, angle: float, width: int = 0):
+        target_rect: pygame.Rect = pygame.Rect(rect)
+        shape_surf = pygame.Surface(target_rect.size, pygame.SRCALPHA)
+        pygame.draw.rect(shape_surf, color, (0, 0, *target_rect.size), width)
+        rotated_surf = pygame.transform.rotate(shape_surf, angle * 180/math.pi)
+        surface.blit(rotated_surf, rotated_surf.get_rect(center = target_rect.topleft))
+        
+    def rotated_ellipse(surface, color: tuple, rect, angle: float, width: int = 0):
+        target_rect = pygame.Rect(rect)
+        shape_surf = pygame.Surface(target_rect.size, pygame.SRCALPHA)
+        pygame.draw.ellipse(shape_surf, color, (0, 0, *target_rect.size), width)
+        rotated_surf = pygame.transform.rotate(shape_surf, angle / math.pi * 180)
+        surface.blit(rotated_surf, rotated_surf.get_rect(center = target_rect.topleft))
+        
+        # Debug
+        pygame.draw.line(surface, (255, 0, 0), (target_rect.x, 0), (target_rect.x, 900), 2)
+        pygame.draw.line(surface, (255, 0, 0), (0, target_rect.y), (900, target_rect.y), 2)
+        pygame.draw.line(surface, (255, 0, 0), (0, 0), (900, 900), 2)
+        pygame.draw.line(surface, (255, 0, 0), (0, 900), (900, 0), 2)
+
+        pygame.draw.circle(surface, (0, 255, 0), (target_rect.x, target_rect.y), 5)
