1141ab47d001f667e5b009e1fd39e75180cd6e6d
E-DeBusk <debusk.elijah@gmail.com>
2024-11-29 16:26:57 -0800

    Created and implemented a tail for the fish using another Inverse Kinematics link

--- v2/game/entity/fish.py
+++ v2/game/entity/fish.py
@@ -6,18 +6,27 @@ from lib.boa.math.kinematics.unconstrained.chain import UIKinematicsChain
 from lib.boa.math.kinematics.unconstrained.solver import UIKinematicsSolver
 import game_manager
 from lib.boa.math.utility import math_util
+from game.entity.tail import Tail
+from game.art.fish_color_config import FishColorConfig
 
 class Fish:
     def __init__(self, color_config, scale: float):
         self.path_entity = PathEntity(EntityType.INDIVIDUAL)
-        self.color_config = color_config
+        self.color_config: FishColorConfig = color_config
         self.scale = scale
-        self.kine_chain = UIKinematicsChain([25, 20, 20, 20, 5, 0])
+        self.kine_chain = UIKinematicsChain([20, 20, 20, 20, 5, 0])
         self.kine_sizes = [2, 5, 10, 12, 12, 10]
+        self.tail = Tail(35, scale)
+        self.curve = 0
         
     def update(self, timestamp):
         self.path_entity.loop(timestamp)
         self.kine_chain.update(self.path_entity.affector_pos)
+        self.tail.update(self.kine_chain.links[0], self.kine_sizes[0])
+        
+        self.curve = 0
+        for link in self.kine_chain.links:
+            self.curve += self.kine_chain.links[len(self.kine_chain.links) - 1].angle - link.angle
         
     def draw(self, surface):        
         forefin_joint = self.kine_chain.links[len(self.kine_chain.links) - 3]
@@ -35,13 +44,14 @@ class Fish:
         postfin_position_1 = postfin_origin + math_util.circ_position(postfin_joint.angle + math.pi / 2, postfin_joint_radius)
         postfin_position_2 = postfin_origin + math_util.circ_position(postfin_joint.angle - math.pi / 2, postfin_joint_radius)
         
-        draw.rotated_ellipse(surface, (0, 0, 0), (forefin_position_1.x, forefin_position_1.y, 28, 13), -forefin_joint.angle - fin_angle)
-        draw.rotated_ellipse(surface, (0, 0, 0), (forefin_position_2.x, forefin_position_2.y, 28, 13), -forefin_joint.angle + fin_angle)
+        draw.rotated_ellipse(surface, self.color_config.accent_color, (forefin_position_1.x, forefin_position_1.y, 28, 13), -forefin_joint.angle - fin_angle)
+        draw.rotated_ellipse(surface, self.color_config.accent_color, (forefin_position_2.x, forefin_position_2.y, 28, 13), -forefin_joint.angle + fin_angle)
         
-        draw.rotated_ellipse(surface, (0, 0, 0), (postfin_position_1.x, postfin_position_1.y, 22, 9), -postfin_joint.angle - fin_angle)
-        draw.rotated_ellipse(surface, (0, 0, 0), (postfin_position_2.x, postfin_position_2.y, 22, 9), -postfin_joint.angle + fin_angle)
+        draw.rotated_ellipse(surface, self.color_config.accent_color, (postfin_position_1.x, postfin_position_1.y, 22, 9), -postfin_joint.angle - fin_angle)
+        draw.rotated_ellipse(surface, self.color_config.accent_color, (postfin_position_2.x, postfin_position_2.y, 22, 9), -postfin_joint.angle + fin_angle)
         
-        draw.kine_chain(surface, self.kine_chain, self.kine_sizes)
+        self.tail.draw(surface, self.color_config.accent_color, self.curve)
+        draw.kine_chain(surface, self.color_config.base_color, self.kine_chain, self.kine_sizes)
         
     def debug_draw(self, surface):
         self.path_entity.debug_draw(surface)
